9755d4fce4032d47949019438725399d15d26f40
Gm <13630497916@163.com>
2022-03-03 09:25:11 +0800

    初始化项目

---
+++ online-course-component/src/main/java/com/course/component/UserComponent.java
@@ -0,0 +1,18 @@
+package com.course.component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * 用户组件
+ *
+ * @author panguangming
+ * @since 2022-01-24
+ */
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class UserComponent {
+
+}
