168f911a920d9a0c474d0e14df884f9fea1e8336
Gm <13630497916@163.com>
2022-03-23 21:29:20 +0800

    图片

--- online-course-server/src/main/java/com/course/server/controller/CategoryController.java
+++ online-course-server/src/main/java/com/course/server/controller/CategoryController.java
@@ -1,13 +1,25 @@
 package com.course.server.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.course.api.entity.Category;
+import com.course.api.vo.server.CategoryVo;
 import com.course.commons.model.Response;
 import com.course.component.cache.CategoryCache;
+import com.course.service.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ma.glasnost.orika.MapperFacade;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * 分类
@@ -22,9 +34,23 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class CategoryController {
     private final CategoryCache categoryCache;
+    private final CategoryService categoryService;
+    private final MapperFacade mapperFacade;
 
     @GetMapping("/list")
     public Response categoryList() {
-        return Response.ok(categoryCache.getCategoryVoList());
+        LambdaQueryWrapper<Category> query = Wrappers.lambdaQuery();
+        query.select(Category::getId, Category::getParentId, Category::getTitle, Category::getDisplayOrder);
+        List<Category> list = categoryService.list(query);
+        Map<Integer, Category> categoryMap = list.stream().collect(Collectors.toMap(Category::getId, Function.identity()));
+        list.removeIf(item -> Objects.equals(item.getParentId(), 0));
+        List<CategoryVo> categoryVoList = list.stream().map(item -> {
+            CategoryVo map = mapperFacade.map(item, CategoryVo.class);
+            Category category = categoryMap.get(item.getParentId());
+            map.setParentTitle(category.getTitle())
+                    .setParentDisplayOrder(category.getDisplayOrder());
+            return map;
+        }).collect(Collectors.toList());
+        return Response.ok(categoryVoList);
     }
 }
